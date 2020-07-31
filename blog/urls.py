bfc9c8b5fa165db14b705abb9fb29d6c5cddfd54
vishal958 <vishalmaurya958@gmail.com>
2020-07-31 14:46:07 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ from . import views
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    #path('', PostListView.as_view(), name='blog-home'),
+    #path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/',
          PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
